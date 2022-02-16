3dff07c2f3cf4c36324cbc1640e08b76ee8cacd1
Christiano0407 <placebo0704@gmail.com>
2022-02-16 08:48:58 -0600

    animation gsap one

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ console.log(btnMenu);
 const listMenu = document.querySelector(".wrapper__burger");
 const burgerList = document.querySelector(".burger-list");
 // Events ==>
+// ==== BTN Menu ====
 btnMenu.addEventListener("click", () => {
   console.log("click burger");
   /* list.classList.remove("hidden"); */
@@ -20,3 +21,32 @@ btnMenu.addEventListener("click", () => {
 });
 
 console.groupEnd();
+// =====  Animation GSAP =======
+const tl = gsap.timeline({ default: { ease: "power2.out" } });
+
+// = Background =
+tl.fromTo(
+  ".background__image",
+  { opacity: 0 },
+  { opacity: 1, delay: 0.51, duration: 0.5 },
+  " < "
+);
+// = Page =
+tl.fromTo(
+  ".page",
+  { opacity: 0 },
+  { opacity: 1, delay: 1, duration: 1 },
+  " < "
+);
+tl.fromTo(
+  ".navigation",
+  { y: "-200", opacity: 0 },
+  { y: "0", opacity: 1, delay: 1.5, duration: 1.5 },
+  " < "
+);
+tl.fromTo(
+  ".active",
+  { y: "-200", opacity: 0 },
+  { y: "0", opacity: 1, delay: 1.5, duration: 1.5 },
+  "<"
+);
